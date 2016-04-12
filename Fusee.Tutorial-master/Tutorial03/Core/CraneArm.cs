6335fa6e999a0fe929c4853138d18e35dfbc0a19
freakadelle <doehler.robert@web.de>
2016-04-12 23:46:41 +0200

    Tut3

---
+++ Fusee.Tutorial-master/Tutorial03/Core/CraneArm.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using static Fusee.Engine.Core.Input;
+
+namespace Fusee.Tutorial.Core
+{
+
+    public class CraneArm
+    {
+        public float yaw;
+        public float pitch;
+        public float speed;
+
+        public float3 target;
+
+        public float4x4 xForm;
+        public float3 position;
+        public float3 scaling;
+        public Mesh mesh;
+
+        public CraneArm()
+        {
+            mesh = new Mesh
+            {
+                Vertices = new[]
+                {
+                    // left, down, front vertex
+                    new float3(-1f, -1f, -1f), // 0  - belongs to left
+                    new float3(-1f, -1f, -1f), // 1  - belongs to down
+                    new float3(-1f, -1f, -1f), // 2  - belongs to front
+
+                    // left, down, back vertex
+                    new float3(-1f, -1f,  1f),  // 3  - belongs to left
+                    new float3(-1f, -1f,  1f),  // 4  - belongs to down
+                    new float3(-1f, -1f,  1f),  // 5  - belongs to back
+
+                    // left, up, front vertex
+                    new float3(-1f,  1f, -1f),  // 6  - belongs to left
+                    new float3(-1f,  1f, -1f),  // 7  - belongs to up
+                    new float3(-1f,  1f, -1f),  // 8  - belongs to front
+
+                    // left, up, back vertex
+                    new float3(-1f,  1f,  1f),  // 9  - belongs to left
+                    new float3(-1f,  1f,  1f),  // 10 - belongs to up
+                    new float3(-1f,  1f,  1f),  // 11 - belongs to back
+
+                    // right, down, front vertex
+                    new float3( 1f, -1f, -1f), // 12 - belongs to right
+                    new float3( 1f, -1f, -1f), // 13 - belongs to down
+                    new float3( 1f, -1f, -1f), // 14 - belongs to front
+
+                    // right, down, back vertex
+                    new float3( 1f, -1f,  1f),  // 15 - belongs to right
+                    new float3( 1f, -1f,  1f),  // 16 - belongs to down
+                    new float3( 1f, -1f,  1f),  // 17 - belongs to back
+
+                    // right, up, front vertex
+                    new float3( 1f,  1f, -1f),  // 18 - belongs to right
+                    new float3( 1f,  1f, -1f),  // 19 - belongs to up
+                    new float3( 1f,  1f, -1f),  // 20 - belongs to front
+
+                    // right, up, back vertex
+                    new float3( 1f,  1f,  1f),  // 21 - belongs to right
+                    new float3( 1f,  1f,  1f),  // 22 - belongs to up
+                    new float3( 1f,  1f,  1f),  // 23 - belongs to back
+
+                },
+                Normals = new[]
+                {
+                    // left, down, front vertex
+                    new float3(-1,  0,  0), // 0  - belongs to left
+                    new float3( 0, -1,  0), // 1  - belongs to down
+                    new float3( 0,  0, -1), // 2  - belongs to front
+
+                    // left, down, back vertex
+                    new float3(-1,  0,  0),  // 3  - belongs to left
+                    new float3( 0, -1,  0),  // 4  - belongs to down
+                    new float3( 0,  0,  1),  // 5  - belongs to back
+
+                    // left, up, front vertex
+                    new float3(-1,  0,  0),  // 6  - belongs to left
+                    new float3( 0,  1,  0),  // 7  - belongs to up
+                    new float3( 0,  0, -1),  // 8  - belongs to front
+
+                    // left, up, back vertex
+                    new float3(-1,  0,  0),  // 9  - belongs to left
+                    new float3( 0,  1,  0),  // 10 - belongs to up
+                    new float3( 0,  0,  1),  // 11 - belongs to back
+
+                    // right, down, front vertex
+                    new float3( 1,  0,  0), // 12 - belongs to right
+                    new float3( 0, -1,  0), // 13 - belongs to down
+                    new float3( 0,  0, -1), // 14 - belongs to front
+
+                    // right, down, back vertex
+                    new float3( 1,  0,  0),  // 15 - belongs to right
+                    new float3( 0, -1,  0),  // 16 - belongs to down
+                    new float3( 0,  0,  1),  // 17 - belongs to back
+
+                    // right, up, front vertex
+                    new float3( 1,  0,  0),  // 18 - belongs to right
+                    new float3( 0,  1,  0),  // 19 - belongs to up
+                    new float3( 0,  0, -1),  // 20 - belongs to front
+
+                    // right, up, back vertex
+                    new float3( 1,  0,  0),  // 21 - belongs to right
+                    new float3( 0,  1,  0),  // 22 - belongs to up
+                    new float3( 0,  0,  1),  // 23 - belongs to back
+                },
+                Triangles = new ushort[]
+                {
+                   0,  6,  3,     3,  6,  9, // left
+                   2, 14, 20,     2, 20,  8, // front
+                  12, 15, 18,    15, 21, 18, // right
+                   5, 11, 17,    17, 11, 23, // back
+                   7, 22, 10,     7, 19, 22, // top
+                   1,  4, 16,     1, 16, 13, // bottom 
+                },
+            };
+
+            position = new float3(0, 0, 0);
+            scaling = new float3(1, 1, 1);
+            xForm = float4x4.Identity;
+
+            speed = 100;
+            yaw = 0;
+            pitch = 0;
+            target = new float3(0,0, 0);
+        }
+
+        public float3 getSize()
+        {
+            return scaling;
+        }
+
+        public float4x4 getScalingMat()
+        {
+            return float4x4.CreateScale(scaling);
+        }
+
+        public void rotateAroundPivot(float3 rot, float3 pivot)
+        {
+            xForm =  float4x4.CreateTranslation(position + pivot)
+                   * float4x4.CreateRotationY(rot.y)
+                   * float4x4.CreateRotationX(rot.x)
+                   * float4x4.CreateRotationZ(rot.z)
+                   * float4x4.CreateTranslation(-pivot);
+        }
+
+        public void update()
+        {
+            pitch += (target.x / 8000.0f) * speed;
+            yaw += (target.y / 15000.0f) * speed;
+            yaw = System.Math.Min(yaw, 0.5f);
+            yaw = System.Math.Max(yaw, -0.5f);
+            target.z--;
+        }
+
+        public void rndNewTarget(Random rnd)
+        {
+            target.x = ((float)(rnd.NextDouble() * 2.0f) - 1);
+            target.y = ((float)(rnd.NextDouble() * 2.0f) - 1);
+            target.z = rnd.Next(100, 200);
+
+            speed = rnd.Next((int)System.Math.Abs(position.x) * 1, (int)System.Math.Abs(position.x) * 100);
+        }
+
+        public Boolean hasReachedTarget()
+        {
+            if (target.z <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
--- Fusee.Tutorial-master/Tutorial03/Core/Tutorial.cs
+++ Fusee.Tutorial-master/Tutorial03/Core/Tutorial.cs
@@ -1,4 +1,6 @@
-﻿using Fusee.Base.Common;
+﻿using System;
+using System.Diagnostics;
+using Fusee.Base.Common;
 using Fusee.Engine.Common;
 using Fusee.Engine.Core;
 using Fusee.Math.Core;
@@ -10,146 +12,82 @@ namespace Fusee.Tutorial.Core
     [FuseeApplication(Name = "Tutorial Example", Description = "The official FUSEE Tutorial.")]
     public class Tutorial : RenderCanvas
     {
-        private Mesh _mesh;
         private const string _vertexShader = @"
             attribute vec3 fuVertex;
-            uniform float alpha;
+            attribute vec3 fuNormal;
+
+            uniform mat4 xform;
+
             varying vec3 modelpos;
+            varying vec3 normal;
 
             void main()
             {
                 modelpos = fuVertex;
-                float s = sin(alpha);
-                float c = cos(alpha);
-                gl_Position = vec4( fuVertex.x * c - fuVertex.z * s, 
-                                    fuVertex.y, 
-                                    fuVertex.x * s + fuVertex.z * c, 
-                                    1.0);
+                normal = fuNormal;
+
+                //modelpos = modelpos / gl_Position.w;
+                gl_Position = xform * vec4(modelpos, 1.0);
             }";
 
         private const string _pixelShader = @"
             #ifdef GL_ES
                 precision highp float;
             #endif
+
             varying vec3 modelpos;
+            varying vec3 normal;
+            uniform vec4 params;
 
             void main()
             {
-                gl_FragColor = vec4(modelpos*0.5 + 0.5, 1);
+                float r = ((normal.x / 2.0) + 0.5) * (params.w / 2.0);
+                float g = ((normal.y / 2.0) + 0.5) * (params.w / 7.0);
+                float b = ((normal.z / 2.0) + 0.5) * (params.w / 15.0);
+                gl_FragColor = vec4(r, g, b, 1.0);
             }";
 
 
-        private IShaderParam _alphaParam;
-        private float _alpha;
+        public static float ASPECT_RATIO;
+        private IShaderParam _xformParam;
+        private IShaderParam _shaderParams;
+        private float4 _params;
+        private CraneArm[] craneArms;
+        private float3 _alpha;
+
+        private Random rnd;
+
+        public float4x4 _xform;
 
         // Init is called on startup. 
         public override void Init()
         {
-            _mesh = new Mesh
-            {
-                Vertices = new[]
-                {
-                    // left, down, front vertex
-                    new float3(-1, -1, -1), // 0  - belongs to left
-                    new float3(-1, -1, -1), // 1  - belongs to down
-                    new float3(-1, -1, -1), // 2  - belongs to front
-
-                    // left, down, back vertex
-                    new float3(-1, -1,  1),  // 3  - belongs to left
-                    new float3(-1, -1,  1),  // 4  - belongs to down
-                    new float3(-1, -1,  1),  // 5  - belongs to back
-
-                    // left, up, front vertex
-                    new float3(-1,  1, -1),  // 6  - belongs to left
-                    new float3(-1,  1, -1),  // 7  - belongs to up
-                    new float3(-1,  1, -1),  // 8  - belongs to front
-
-                    // left, up, back vertex
-                    new float3(-1,  1,  1),  // 9  - belongs to left
-                    new float3(-1,  1,  1),  // 10 - belongs to up
-                    new float3(-1,  1,  1),  // 11 - belongs to back
-
-                    // right, down, front vertex
-                    new float3( 1, -1, -1), // 12 - belongs to right
-                    new float3( 1, -1, -1), // 13 - belongs to down
-                    new float3( 1, -1, -1), // 14 - belongs to front
-
-                    // right, down, back vertex
-                    new float3( 1, -1,  1),  // 15 - belongs to right
-                    new float3( 1, -1,  1),  // 16 - belongs to down
-                    new float3( 1, -1,  1),  // 17 - belongs to back
-
-                    // right, up, front vertex
-                    new float3( 1,  1, -1),  // 18 - belongs to right
-                    new float3( 1,  1, -1),  // 19 - belongs to up
-                    new float3( 1,  1, -1),  // 20 - belongs to front
-
-                    // right, up, back vertex
-                    new float3( 1,  1,  1),  // 21 - belongs to right
-                    new float3( 1,  1,  1),  // 22 - belongs to up
-                    new float3( 1,  1,  1),  // 23 - belongs to back
-
-                },
-                Normals = new[]
-                {
-                    // left, down, front vertex
-                    new float3(-1,  0,  0), // 0  - belongs to left
-                    new float3( 0, -1,  0), // 1  - belongs to down
-                    new float3( 0,  0, -1), // 2  - belongs to front
-
-                    // left, down, back vertex
-                    new float3(-1,  0,  0),  // 3  - belongs to left
-                    new float3( 0, -1,  0),  // 4  - belongs to down
-                    new float3( 0,  0,  1),  // 5  - belongs to back
-
-                    // left, up, front vertex
-                    new float3(-1,  0,  0),  // 6  - belongs to left
-                    new float3( 0,  1,  0),  // 7  - belongs to up
-                    new float3( 0,  0, -1),  // 8  - belongs to front
-
-                    // left, up, back vertex
-                    new float3(-1,  0,  0),  // 9  - belongs to left
-                    new float3( 0,  1,  0),  // 10 - belongs to up
-                    new float3( 0,  0,  1),  // 11 - belongs to back
-
-                    // right, down, front vertex
-                    new float3( 1,  0,  0), // 12 - belongs to right
-                    new float3( 0, -1,  0), // 13 - belongs to down
-                    new float3( 0,  0, -1), // 14 - belongs to front
-
-                    // right, down, back vertex
-                    new float3( 1,  0,  0),  // 15 - belongs to right
-                    new float3( 0, -1,  0),  // 16 - belongs to down
-                    new float3( 0,  0,  1),  // 17 - belongs to back
-
-                    // right, up, front vertex
-                    new float3( 1,  0,  0),  // 18 - belongs to right
-                    new float3( 0,  1,  0),  // 19 - belongs to up
-                    new float3( 0,  0, -1),  // 20 - belongs to front
-
-                    // right, up, back vertex
-                    new float3( 1,  0,  0),  // 21 - belongs to right
-                    new float3( 0,  1,  0),  // 22 - belongs to up
-                    new float3( 0,  0,  1),  // 23 - belongs to back
-                },
-                Triangles = new ushort[]
-                {
-                   0,  6,  3,     3,  6,  9, // left
-                   2, 14, 20,     2, 20,  8, // front
-                  12, 15, 18,    15, 21, 18, // right
-                   5, 11, 17,    17, 11, 23, // back
-                   7, 22, 10,     7, 19, 22, // top
-                   1,  4, 16,     1, 16, 13, // bottom 
-                },
-            };
-
             var shader = RC.CreateShader(_vertexShader, _pixelShader);
             RC.SetShader(shader);
-            _alphaParam = RC.GetShaderParam(shader, "alpha");
-            _alpha = 0;
+
+            _xformParam = RC.GetShaderParam(shader, "xform");
+            _shaderParams = RC.GetShaderParam(shader, "params");
 
             // Set the clear color for the backbuffer
-            RC.ClearColor = new float4(0.1f, 0.3f, 0.2f, 1);
+            RC.ClearColor = new float4(0.1f, 0.1f, 0.1f, 1);
+
+            Tutorial.ASPECT_RATIO = Width/(float) Height;
+
+            craneArms = new CraneArm[50];
+
+            for (int i = 0; i < craneArms.Length; i++)
+            {
+                craneArms[i] = new CraneArm();
+                float length = 0.05f;
+                craneArms[i].scaling = new float3(length, 0.5f / i, 0.5f / i);
+                //craneArms[i].position = new float3((i * length) * 1f, 0, 0);
+                craneArms[i].position = new float3( 0.5f + (i * length) * 2f, 0, 0);
+            }
+
+            _alpha = new float3(0, 0, 0);
+            _params = new float4(0,0,0,0);
+            rnd = new Random();
+
         }
 
         // RenderAFrame is called once a frame
@@ -159,15 +97,71 @@ namespace Fusee.Tutorial.Core
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
 
             float2 speed = Mouse.Velocity;
-            _alpha += speed.x * 0.0001f;
-            RC.SetShaderParam(_alphaParam, _alpha);
 
-            RC.Render(_mesh);
+            if (Mouse.LeftButton || Touch.GetTouchActive(TouchPoints.Touchpoint_0))
+            {
+                //Rotate arounf x and y axis by dragging the mouse/touch Input
+                _alpha.x += speed.x * -0.0001f;
+                _alpha.y += speed.y*-0.0001f;
+            }
+
+            craneArms[0].yaw += Keyboard.ADAxis * 0.1f;
+            craneArms[0].pitch += Keyboard.WSAxis * 0.1f;
+            craneArms[1].yaw += Keyboard.LeftRightAxis * 0.1f;
+            craneArms[1].pitch += Keyboard.UpDownAxis * 0.1f;
+
+            // Setup View
+            var projection = float4x4.CreatePerspectiveFieldOfView((float) System.Math.PI * 0.5f, Tutorial.ASPECT_RATIO, 0.01f, 50);
+
+            _alpha.x = System.Math.Max(_alpha.x, (float) -System.Math.PI);
+            _alpha.x = System.Math.Min(_alpha.x, (float) System.Math.PI);
+            var view = float4x4.CreateTranslation(-1.5f, 0, 3f) * float4x4.CreateRotationY(_alpha.x) * float4x4.CreateRotationX(_alpha.y);
+
+            //Apply Matrice Changes to CraneArms
+            float4x4 parent_xForm = float4x4.Identity;
+
+            //First manual Arm
+            craneArms[0].rotateAroundPivot(new float3(craneArms[0].pitch, craneArms[0].yaw, 0), new float3(-craneArms[0].scaling.x, 0, 0));
+            _xform = projection * view * parent_xForm * craneArms[0].xForm * craneArms[0].getScalingMat();
+            RC.SetShaderParam(_xformParam, _xform);
+            RC.Render(craneArms[0].mesh);
+            parent_xForm = parent_xForm * craneArms[0].xForm * float4x4.CreateTranslation(-craneArms[0].position);
+
+            //Second Manual Arm
+            craneArms[1].rotateAroundPivot(new float3(craneArms[1].pitch, craneArms[1].yaw, 0), new float3(-craneArms[1].scaling.x, 0, 0));
+            _xform = projection * view * parent_xForm * craneArms[1].xForm * craneArms[1].getScalingMat();
+            RC.SetShaderParam(_xformParam, _xform);
+            RC.Render(craneArms[1].mesh);
+            parent_xForm = parent_xForm * craneArms[1].xForm * float4x4.CreateTranslation(-craneArms[1].position);
+
+            for (int i = 2; i < craneArms.Length; i++)
+            {
+                if (craneArms[i].hasReachedTarget())
+                {
+                    craneArms[i].rndNewTarget(rnd);
+                }
+                else
+                {
+                    craneArms[i].update();
+                }
+
+                craneArms[i].rotateAroundPivot(new float3(craneArms[i].pitch, craneArms[i].yaw, 0),
+                    new float3(-craneArms[i].scaling.x, 0, 0));
+                _xform = projection*view*parent_xForm*craneArms[i].xForm*craneArms[i].getScalingMat();
+
+                _params.w = craneArms[i].position.x;
+                _params.xyz = craneArms[i].target;
+
+                RC.SetShaderParam(_xformParam, _xform);
+                RC.SetShaderParam(_shaderParams, _params);
+                RC.Render(craneArms[i].mesh);
+
+                parent_xForm = parent_xForm*craneArms[i].xForm*float4x4.CreateTranslation(-craneArms[i].position);
+            }
 
             // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
             Present();
         }
-
 
         // Is called when the window was resized
         public override void Resize()
@@ -177,12 +171,20 @@ namespace Fusee.Tutorial.Core
 
             // Create a new projection matrix generating undistorted images on the new aspect ratio.
             var aspectRatio = Width/(float) Height;
+            Tutorial.ASPECT_RATIO = Width / (float)Height;
 
             // 0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
             // Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
             // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
             var projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 20000);
             RC.Projection = projection;
+        }
+
+        static float NextFloat(Random random)
+        {
+            double mantissa = (random.NextDouble() * 2.0) - 1.0;
+            double exponent = System.Math.Pow(2.0, random.Next(-126, 128));
+            return (float)(mantissa * exponent);
         }
 
     }
